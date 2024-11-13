86f1842fb48246e45a75ed998102d7f4c76aff97
Alfredo <alfredrz2012@gmail.com>
2024-11-12 20:48:38 -0500

    Refactor Stripe integration to use a getter for Stripe instance and clean up onLoad method

--- modules/integrations/entities/stripe/stripe.js
+++ modules/integrations/entities/stripe/stripe.js
@@ -4,30 +4,34 @@ import { BaseDocument } from 'loopar';
 import StripeClass from 'stripe';
 
 export default class Stripe extends BaseDocument {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  get Stripe() {
+    return StripeClass(this.secret_key);
+  }
+
+  async onLoad() {
+    await super.onLoad();
+    //this.stripe = StripeClass(this.secret_key);
+  }
+
+  async authorize() {
+    return {
+      client_secret: await this.clientSecret(),
+      publishable_key: this.public_key,
     }
+  }
 
-    async onLoad() {
-        await super.onLoad();
-        this.stripe = StripeClass(this.secret_key);
-    }
+  async clientSecret() {
+    const paymentIntent = await this.Stripe().paymentIntents.create({
+      amount: 5,
+      currency: 'usd'
+    });
 
-    async authorize() {
-        return {
-            client_secret: await this.clientSecret(),
-            publishable_key: this.public_key,
-        }
-    }
+    console.log({ paymentIntent });
 
-    async clientSecret() {
-        const paymentIntent = await this.stripe.paymentIntents.create({
-            amount: 5,
-            currency: 'usd'
-        });
-
-        console.log({ paymentIntent });
-
-        return paymentIntent.client_secret;
-    }
+    return paymentIntent.client_secret;
+  }
 }
